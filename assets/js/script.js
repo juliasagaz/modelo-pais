e1129555b320d392af70f7129a237da9f305ba0a
juliasagaz <juliasagazcorrea2004@gmail.com>
2023-09-11 16:59:14 -0300

    git add: primeiro commit

---
+++ assets/js/script.js
@@ -0,0 +1,5 @@
+// Simulate random data for IDH, health, education, and security
+document.getElementById("idh-value").textContent = (Math.random() * 0.3 + 0.6).toFixed(3);
+document.getElementById("vida-value").textContent = (Math.random() * 10 + 70).toFixed(1) + " anos";
+document.getElementById("alfabetizacao-value").textContent = (Math.random() * 10 + 90).toFixed(1) + "%";
+document.getElementById("criminalidade-value").textContent = (Math.random() * 30 + 10).toFixed(1);
